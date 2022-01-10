4d5f8f122e3884808c4b669001f03cab595896b6
Kyle Smith <kyle.smith@polymathian.com>
2022-01-10 15:55:09 +1000

    First release test

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const aws = require('aws-sdk');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // Get input
+    const domainName = core.getInput("domain", {required: true});
+    const packageName = core.getInput("package", {required: true});
+    const repositoryName = core.getInput("repository", {required: true});
+    const format = core.getInput("format", {required: false}) || "npm";
+    const prefix = core.getInput("prefix", {required: false});
+    const increment = core.getInput("increment-amount", {required: false}) || 1;
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // Create required resources
+    const ca = new aws.CodeArtifact();
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info(`Fetching latest package version for ${repositoryName}:${packageName}`);
+    const listVersionsResp = await ca.listPackageVersions({
+      domain: domainName,
+      format,
+      package: packageName,
+      repository: repositoryName,
+    }).promise();
+
+    let fetchedVersion;
+    if (prefix) {
+      // Search for the first (latest) version matching the prefix.
+      for (const version in listVersionsResp.versions) {
+        if (version.version.startsWith(prefix)) {
+          fetchedVersion = version.version;
+          break;
+        }
+      }
+
+      // No version was found that matched the prefix.
+      if (!fetchedVersion) {
+        throw new Error(`Failed to locate version matching prefix ${prefix}`);
+      }
+    } else {
+      fetchedVersion = listVersionsResp.defaultDisplayVersion.split(".");
+    }
+
+    let versionParts = fetchedVersion.split(".");
+    const lastIndex = versionParts.length - 1;
+
+    versionParts[lastIndex] = String(Number(versionParts[lastIndex]) + Number(increment));
+    const versionString = versionParts.join(".");
+
+    core.info(`Fetched package version ${fetchedVersion}. New version ${versionString}`);
+
+    core.setOutput('version_number', versionString);
   } catch (error) {
     core.setFailed(error.message);
   }
